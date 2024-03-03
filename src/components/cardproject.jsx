28a7f52deecd4c70f317dd6c55ff3c69d566b7cb
Omar Reyes Zamudio <72781778+omar49511@users.noreply.github.com>
2024-03-02 22:56:37 -0800

    cambios

--- src/components/cardproject.jsx
+++ src/components/cardproject.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function CardProject({
   image,
@@ -13,7 +14,13 @@ export default function CardProject({
       className="relative group duration-500 cursor-pointer group overflow-hidden relative  h-72 w-56 rounded hover:duration-700 duration-700 "
     >
       <div className="w-full h-72  text-white">
-        <img className="w-full h-full object-cover" src={image} alt={title} />
+        <Image
+          className="w-full h-full object-cover"
+          width={300}
+          height={300}
+          src={image}
+          alt={title}
+        />
       </div>
       <div className="absolute bg-[#262626ff] -bottom-[100%] w-full p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
         <div>
