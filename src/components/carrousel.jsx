1d7ef6abc9a73b3b7b3686af4834206d9cef48e2
Omar Reyes Zamudio <72781778+omar49511@users.noreply.github.com>
2024-03-02 23:38:45 -0800

    cambio

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -8,56 +8,6 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 
 import { FreeMode, Pagination, Navigation } from "swiper/modules";
-// let cardDataList = [
-//   {
-//     id: 1,
-//     image:
-//       "https://i.pinimg.com/564x/5c/7e/39/5c7e393665b3643bc3e68975fb2ef335.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     id: 2,
-//     image:
-//       "https://i.pinimg.com/564x/79/61/e0/7961e093496d0b3940bed1ffa2e13523.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     id: 3,
-//     image:
-//       "https://i.pinimg.com/564x/cc/eb/dc/ccebdcfbf20712a20c22e04f23d9ff72.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     id: 4,
-//     image:
-//       "https://i.pinimg.com/564x/9d/cc/29/9dcc29cdcb1deb851939d9330657245b.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     id: 5,
-//     image:
-//       "https://i.pinimg.com/564x/03/95/f0/0395f09e89eabcbb89bc0b3e33a52ccd.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-//   {
-//     id: 6,
-//     image:
-//       "https://i.pinimg.com/564x/fa/85/a7/fa85a7b7fdc7e912ffaaad8aecb9d9c4.jpg",
-//     Cardtag: "Next Js",
-//     title: "Cheat Sheet",
-//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//   },
-// ];
 
 export default function Carrousel() {
   const [cardDataList, setCardDataList] = useState([]);
@@ -78,7 +28,7 @@ export default function Carrousel() {
     };
 
     fetchFromNotion();
-  }, []);
+  }, [apiUrl]);
   return (
     <div className="max-w-[90%] mx-auto">
       <Swiper
