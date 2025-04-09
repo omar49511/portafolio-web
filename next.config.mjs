4e7f77a811c41d2b169a744c5c1381541dab2f48
omar49511 <omar.reyesz18@tectijuana.edu.mx>
2025-04-09 06:51:52 -0700

    feat: add i.ytimg.com to image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.pinimg.com", "www.notion.so"], // Agrega aquí el dominio de tu imagen externa
+    domains: ["i.pinimg.com", "www.notion.so", "i.ytimg.com"], // Agrega aquí el dominio de tu imagen externa
   },
 };
+
+export default nextConfig;
